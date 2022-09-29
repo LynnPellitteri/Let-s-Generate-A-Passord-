d40dfa2c1e98f04a6b0dede379f71e0ee76a4c83
LynnPellitteri <lynnpellitteri@gmail.com>
2022-09-29 18:12:13 -0400

    linked JS to HTML

---
+++ assets/script.js
@@ -0,0 +1,49 @@
+
+// Assignment code here
+
+
+var=
+var=
+var=
+
+
+
+
+
+function criteriaSelect() {
+
+
+
+
+}
+
+function buttonClick() {
+prompt("first prompt")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function// Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);git add
