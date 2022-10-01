331282d9aaba553d45b73ff76ef7fb0664d2b885
LynnPellitteri <lynnpellitteri@gmail.com>
2022-10-01 10:47:32 -0400

    very rough formulation added pics and original read me to save time

--- assets/script.js
+++ assets/script.js
@@ -2,35 +2,44 @@
 // Assignment code here
 
 
-var=
-var=
-var=
 
+//items 
 
+var upperCase "A, B, C, D, E, F, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
+var lowerCase "a, b, c, d, e, f, g, h, i, j, l, m, n ,o, p, q, r, s, t, u, v, w, x, y, z"
+var special   "$,  _"
+var password.length: 8-128
 
-
-
-function criteriaSelect() {
-
-
-
-
-}
+var password()["uppercase" "lowercase" numbers "special character" "length"];
 
 function buttonClick() {
-prompt("first prompt")
 }
 
+f
+
+// steps
+//user clicks button
+//uppercase yes no
+
+
+if lowercase and numbers are true
+the special characters and length 8-128 are ture
+else less than 8 is false
+
+
+prompt password length 8-128
+//prompt types of characters
+//yes or no to include lowercase uppercase numrtic and/or special characters
+//st least one character type is included
+// answer all prompts password id generated
+//password generated to the page
 
 
 
 
 
-
-
-
-
-
+//call the function  
+ 
 
 
 // function// Get references to the #generate element
@@ -38,8 +47,10 @@ prompt("first prompt")
 
 // // Write password to the #password input
 // function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+//   var password = generatePassword 
+ var generate password = math.floor(math.random( ) * options.length)
+ 
+var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
 
