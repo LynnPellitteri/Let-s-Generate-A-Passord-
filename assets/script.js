0d40158eb2aa3013a35d33269980530f968c2739
LynnPellitteri <lynnpellitteri@gmail.com>
2022-10-01 14:55:16 -0400

    changes after testing

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,69 @@
 
 // Assignment code here
 
-
-
-//items 
-
-var upperCase "A, B, C, D, E, F, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z"
-var lowerCase "a, b, c, d, e, f, g, h, i, j, l, m, n ,o, p, q, r, s, t, u, v, w, x, y, z"
-var special   "$,  _"
-var password.length: 8-128
-
-var password()["uppercase" "lowercase" numbers "special character" "length"];
-
-function buttonClick() {
-}
-
-f
-
 // steps
 //user clicks button
 //uppercase yes no
 
 
-if lowercase and numbers are true
-the special characters and length 8-128 are ture
-else less than 8 is false
+//if lowercase and numbers are true
+//the special characters and length 8-128 are ture
+//else less than 8 is false
 
 
-prompt password length 8-128
+//prompt password length 8-128
 //prompt types of characters
 //yes or no to include lowercase uppercase numrtic and/or special characters
 //st least one character type is included
 // answer all prompts password id generated
 //password generated to the page
 
+//items 
+
+var upperCase= "A, B, C, D, E, F, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z";
+var lowerCase= "a, b, c, d, e, f, g, h, i, j, l, m, n ,o, p, q, r, s, t, u, v, w, x, y, z";
+var special="!,@,#,$, %,^, &,* ,(,),-,  _,+,=,~,`,<,>,/,?,{,},[,],|,";
+var numbers=8-128;
+
+if( numbers.length > 8){ //is true
+    the special characters and length 8-128 //are ture
+     < 8 //is false
+}
+
+var password="uppercase,lowercase, special,numbers.length";
 
 
 
+window.prompt("upperCase yes or no")
+
+window.prompt("lowerCase yes or no")
+
+window.prompt("spcecial characters yes or no")
+
+window.prompt("pasword length")
 
 //call the function  
  
+//var password="generate password";
+//var generate password= math.floor(math.random( ) * options.length)
 
+//function buttonclick( passWord) {
+    //generate password=[(math.floor(math.random))* (numbers.length)]
+//}
 
 // function// Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
+ //var generateBtn = document.querySelector("#generate");
+
+for button click ("generate password ")';'
+
+}
 
 // // Write password to the #password input
 // function writePassword() {
 //   var password = generatePassword 
- var generate password = math.floor(math.random( ) * options.length)
+ ///var generate password = math.floor(math.random( ) * options.length)
  
-var passwordText = document.querySelector("#password");
+//var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
 
